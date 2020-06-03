022470c5282e59c643cabd35f57959e8cc1b3121
ben <ben.wallace@protonmail.ch>
2020-06-02 22:06:05 -0400

    starting

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,13 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
+    {
+      resolve: `gatsby-source-prismic-graphql`,
+      options: {
+        repositoryName: `benwal-resume`
+      }
+    },
+    `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
